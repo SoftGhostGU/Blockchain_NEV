3a2d965091b17af5d1a32d5b6a2e7c204ac65e8b
GHOST <3089308393@qq.com>
2025-02-03 00:25:39 +0800

    move photos of UserHomePage

--- fronted/src/router/index.ts
+++ fronted/src/router/index.ts
@@ -3,7 +3,7 @@ import Login from '../component/Login.vue';
 import Register from '../component/Register.vue';
 // import NotFound from '../component/NotFound.vue';
 import DriverHomePage from '../component/DriverHomePage.vue';
-import UserHomePage from '../component/UserHomePage.vue';
+import UserHomePage from '../component/UserHomePage/UserHomePage.vue';
 
 const routes = [
   {
