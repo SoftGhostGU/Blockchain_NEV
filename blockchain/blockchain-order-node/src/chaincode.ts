1c98324f1ebce2afe1821b7309b1e412c645326d
GHOST <3089308393@qq.com>
2025-07-13 00:16:02 +0800

    feat: 基本完成node.js版本的chaincode

--- blockchain/blockchain-order-node/src/chaincode.ts
+++ blockchain/blockchain-order-node/src/chaincode.ts
@@ -9,6 +9,7 @@ export class Chaincode implements ChaincodeInterface {
     public async Init(stub: ChaincodeStub): Promise<any> {
         const { fcn, params } = stub.getFunctionAndParameters();
         console.info('Init()', fcn, params);
+        await this.initLedger(stub);
         return Shim.success();
     }
 
@@ -23,8 +24,39 @@ export class Chaincode implements ChaincodeInterface {
         }
     }
 
+    public async initLedger(stub: ChaincodeStub): Promise<any> {
+        console.log("============= START : Initialize Ledger ============");
+        const orders = [
+            {
+                ownerId: "1",
+                orderId: "1",
+                userId: "101",
+                price: 100.0
+            }, {
+                ownerId: "2",
+                orderId: "2",
+                userId: "102",
+                price: 200.0
+            }, {
+                ownerId: "3",
+                orderId: "3",
+                userId: "103",
+                price: 300.0
+            }
+        ]
+
+        for (const order of orders) {
+            const orderKey = "ORDER" + order.orderId;
+            const orderValue = JSON.stringify(order);
+            await stub.putState(orderKey, new Uint8Array(Buffer.from(orderValue)));
+            console.info(`Order ${orderKey} added to the ledger`);
+        }
+
+        console.log("============= END : Initialize Ledger ============");
+    }
+
     // query order by id
-    public async Query(stub: ChaincodeStub, args: string[]): Promise<any> {
+    public async queryById(stub: ChaincodeStub, args: string[]): Promise<any> {
         if (args.length !== 1) {
             throw new Error('Incorrect number of arguments. Expecting 1');
         }
@@ -38,8 +70,27 @@ export class Chaincode implements ChaincodeInterface {
         }
     }
 
-    // owner accept order from user
-    public async acceptOrder(stub: ChaincodeStub, args: string[]) {
+    // query all orders
+    public async queryAll(stub: ChaincodeStub): Promise<any> {
+        const iterator = await stub.getStateByRange('', ''); // 传两个空值，代表遍历所有键值对
+        const allOrders: any[] = [];
+        let res = await iterator.next();
+
+        while (!res.done) {
+            const order = JSON.parse(res.value.toString());
+            allOrders.push(order);
+            res = await iterator.next();
+        }
+
+        await iterator.close();
+
+        console.log(`Found ${allOrders.length} orders`);
+        console.log(allOrders);
+        return Shim.success(Buffer.from(JSON.stringify(allOrders)));
+    }
+
+    // record order that owner accepted from user
+    public async recordOrder(stub: ChaincodeStub, args: string[]) {
         // `ownerId` accept the `order(id)` from `userId` with `price`
         const ownerId = args[0];
         const orderId = args[1];
