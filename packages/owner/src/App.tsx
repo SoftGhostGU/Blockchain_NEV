b6cdc37515ddf4cc5fb8ae911a2016f06d4308de
SoftGhostGU <163658635+SoftGhostGU@users.noreply.github.com>
2025-08-21 23:35:54 +0800

    feat: 添加路由，完成相关配置

--- packages/owner/src/App.tsx
+++ packages/owner/src/App.tsx
@@ -1,13 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
-import './App.css'
+import Login from './pages/Login';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <MainLayout />
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<MainLayout />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
