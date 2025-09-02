d907abed7c4be0dbb6e5d6f5068da26775d8047b
SoftGhostGU <163658635+SoftGhostGU@users.noreply.github.com>
2025-09-02 23:52:53 +0800

    feat: App.vue，导入必要的pnpm包，并在main.js中引入App.vue文件

---
+++ packages/admin/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <n-config-provider>
+    <n-message-provider>
+      <router-view />
+    </n-message-provider>
+  </n-config-provider>
+</template>
+
+<script setup>
+import { NConfigProvider, NMessageProvider } from "naive-ui"
+</script>
+
+<style>
+/* 全局样式或 App 容器样式 */
+#app {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
